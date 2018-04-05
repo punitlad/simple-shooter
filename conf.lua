51aec687adbb0b04bbfae43daab7e308f91ebbf3
Punit Lad <plad@thoughtworks.com>
2018-04-05 13:58:24 -0400

    init love project

---
+++ conf.lua
@@ -0,0 +1,9 @@
+-- http://www.love2d.org/wiki/Config_Files
+function love.conf(t)
+    t.title = "Scrolling shooter tutorial"
+    t.version = "0.1"
+    t.window.width = 480
+    t.window.height = 800
+
+    t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,12 @@
+-- game logic lives here
+function love.load() -- on startup
+
+end
+
+function love.update(deltaTime) -- on every frame 
+
+end
+
+function love.draw(deltaTime) -- on every frame
+    
+end

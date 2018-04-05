a6136bb517738d28a3167f856b37b069ed739b60
Punit Lad <plad@thoughtworks.com>
2018-04-05 14:24:22 -0400

    make player move left and right

--- main.lua
+++ main.lua
@@ -6,7 +6,16 @@ function love.load() -- on startup
 end
 
 function love.update(deltaTime) -- on every frame 
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
 
+    movement = player.speed*deltaTime
+    if love.keyboard.isDown('left', 'a') then
+        player.x = player.x - movement
+    elseif love.keyboard.isDown('right', 'd') then
+        player.x = player.x + movement
+    end
 end
 
 function love.draw(deltaTime) -- on every frame
